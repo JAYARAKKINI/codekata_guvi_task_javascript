e6703d8979e91b83a6d4af5a90021a251cecb268
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-05-06 03:59:16 +0530

    Create concat_function_alternate.js

---
+++ map_filter_every_index_concat_sum_fun/concat_function_alternate.js
@@ -0,0 +1,10 @@
+//using concat() function
+
+const str1 = 'Hello';
+const str2 = str1.concat(' ', 'World');
+console.log(str2)
+
+
+//without using concat() function
+
+console.log(['Hello', ' ', 'World'].join('')); 
