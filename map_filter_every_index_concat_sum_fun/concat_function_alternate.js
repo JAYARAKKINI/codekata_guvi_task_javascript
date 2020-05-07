2ccce2750c92db9c4b01939767199bcb01b90ae5
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-05-07 19:40:31 +0530

    Update concat_function_alternate.js

--- map_filter_every_index_concat_sum_fun/concat_function_alternate.js
+++ map_filter_every_index_concat_sum_fun/concat_function_alternate.js
@@ -1,7 +1,7 @@
 const concate = (arr1,arr2) => {
-  for(var i=0;i<arr2.length;i++){
+  for(var i=0;i<2;i++){
     arr1.push(arr2[i]);
   }
-  return arr1
+  return arr1.join(" ")
 }
 console.log(concate([1,2,3,4],[5,6,7,8]))
