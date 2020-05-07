ec75d3fb272afd2bedf70422fcd23379aa88cc5d
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-05-07 19:18:06 +0530

    Update findindex_function_alternate.js

--- map_filter_every_index_concat_sum_fun/findindex_function_alternate.js
+++ map_filter_every_index_concat_sum_fun/findindex_function_alternate.js
@@ -1,24 +1,10 @@
-//using findIndex() function
-
-var ages = [3, 10, 18, 20];
-
-function checkAdult(age) {
-  return age >= 18;
+const index = (array, callback) => {
+  for(var i=0;i<array.length;i++){
+    if(callback(array[i]))
+      {
+        return i
+      }
+  }
 }
-
-function myFunction() {
-  console.log(ages.findIndex(checkAdult));
-}
-
-
-//without using findIndex() function
-
-var ages = [3, 10, 18, 20];
-var age=[]
-for(i=0;i<ages.length;i++){
-    if(ages[i]>=18)
-    {
-     age.push(i)
-    }
-}
-console.log(age[0])
+const indexval =(value)=>value==2
+console.log(index([2,3,5,8,12], indexval))
