4c9f6f5cec4651ca9c475ce927f6678e8e1383a6
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-05-11 13:43:29 +0530

    Create 03_multiple_of_7.js

---
+++ Mathematics/03_multiple_of_7.js
@@ -0,0 +1,8 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  if(data%7===0)
+    console.log("yes");
+  else
+    console.log("no");
+});
