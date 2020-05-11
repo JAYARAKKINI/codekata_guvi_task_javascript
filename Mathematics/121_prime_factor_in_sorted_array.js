9f0110d334d4fcaf109597cc0ddc242846f1a36e
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-05-11 12:51:22 +0530

    Update 121_prime_factor_in_sorted_array.js

--- Mathematics/121_prime_factor_in_sorted_array.js
+++ Mathematics/121_prime_factor_in_sorted_array.js
@@ -1,16 +1,26 @@
 const readline=require("readline");
 const inp=readline.createInterface({input:process.stdin});
 inp.on("line",(data)=>{
-   var a=data.split("") ;
-   var b;
-   var c=[];
-   var sum=0;
-   for(i=0;i<a.length;i++){
-       b=Math.pow(parseInt(a[i]),parseInt(i))
-       c.push(b)
-   }
-   for(j=0;j<c.length;j++){
-       sum=parseInt(sum)+parseInt(c[j])
-   }
-   console.log(sum)
+    var num=data
+
+    var primeFactors = [];
+    while (num % 2 === 0) {
+        primeFactors.push(2);
+        num = num / 2;
+    }
+    
+    var sqrtNum = Math.sqrt(num);
+    for (var i = 3; i <= sqrtNum; i++) {
+        while (parseInt(num) % parseInt(i) === 0) {
+            primeFactors.push(i);
+            num = num / i;
+        }
+    }
+
+    if (num > 2) {
+        primeFactors.push(num);
+    }
+    var pf=primeFactors;
+    let uni=[...new Set(pf)];
+    console.log(uni.join(" "));
 });
